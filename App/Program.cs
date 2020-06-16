c70932e1f83d92a3a3616d7c70eb58e3f51b7605
Oli Wennell <oli.wennell@gmail.com>
2020-06-16 22:12:20 +0100

    :tada: The game state can be rendered

---
+++ App/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace App
+{
+    public class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+    
+    public class Shape
+    {
+        public Point[] Points { get; }
+
+        public Shape(IEnumerable<Point> points) =>
+            Points = points.ToArray();
+    }
+
+    public static class Rendering
+    {
+        public static string[] RenderState(State state)
+        {
+            var rows = new char[state.Height][];
+
+            for (var row = 0; row < state.Height; row++)
+            {
+                rows[row] = new char[state.Width];
+                for (var column = 0; column < state.Width; column++)
+                    rows[row][column] = ' ';
+            }
+
+            var allPoints = state.StaticShapes.SelectMany(s => s.Points)
+                .Concat(state.FallingShape.Points);
+            
+            foreach (var point in allPoints)
+            {
+                rows[point.Y][point.X] = 'X';
+            }
+            
+            return rows.Select(r => new string(r))
+                       .ToArray();
+        }
+    }
+    
+    public class State
+    {
+        public uint Width { get; }
+        public uint Height { get; }
+        
+        public Shape[] PendingShapes { get; }
+        public Shape FallingShape { get; }
+        public Shape[] StaticShapes { get; }
+
+        public State(
+            uint width,
+            uint height,
+            IEnumerable<Shape> pendingShapes,
+            Shape fallingShape,
+            IEnumerable<Shape> staticShapes)
+        {
+            Width = width;
+            Height = height;
+            FallingShape = fallingShape;
+            StaticShapes = staticShapes.ToArray();
+        }
+    }
+    
+    public static class Game
+    {
+        public static State Step(State state)
+        {
+            return null;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Tests/GameTests.cs
@@ -0,0 +1,117 @@
+using App;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class GameTests
+    {
+        [Test]
+        public void End_to_end()
+        {
+            
+        }
+    }
+
+    public class RenderTests
+    {
+        [Test]
+        public void The_falling_shape_is_rendered_as_text()
+        {
+            var state = new State(
+                width: 4,
+                height: 5,
+                pendingShapes: new Shape[0], 
+                fallingShape: new Shape(new []
+                {
+                    new Point(1,2),
+                    new Point(2,2),
+                    new Point(1,1),
+                    new Point(2,1)
+                }),
+                staticShapes: new Shape[0]
+            );
+
+            var renderedText = Rendering.RenderState(state);
+            
+            Assert.That(renderedText, Is.EquivalentTo(new[]
+            {
+                "    ",
+                "    ",
+                " XX ",
+                " XX ",
+                "    ",
+            }));
+        }
+        
+        [Test]
+        public void Static_shapes_are_rendered_as_text()
+        {
+            var state = new State(
+                width: 5,
+                height: 5,
+                pendingShapes: new Shape[0], 
+                fallingShape: new Shape(new Point[0]), 
+                staticShapes: new []
+                {
+                    new Shape(new []
+                    {
+                        new Point(1,1),
+                        new Point(2,1),
+                        new Point(1,0),
+                        new Point(2,0)
+                    }),
+                    new Shape(new []
+                    {
+                        new Point(1,3),
+                        new Point(2,3),
+                        new Point(3,3),
+                        new Point(4,3)
+                    })
+                }
+            );
+
+            var renderedText = Rendering.RenderState(state);
+            
+            Assert.That(renderedText, Is.EquivalentTo(new[]
+            {
+                "     ",
+                " XXXX",
+                "     ",
+                " XX  ",
+                " XX  ",
+            }));
+        }
+        
+        [Test]
+        public void Pending_shapes_are_not_rendered()
+        {
+            var state = new State(
+                width: 4,
+                height: 5,
+                pendingShapes: new []
+                {
+                    new Shape(new []
+                    {
+                        new Point(1,1),
+                        new Point(2,1),
+                        new Point(1,0),
+                        new Point(2,0)
+                    })
+                },
+                fallingShape: new Shape(new Point[0]), 
+                staticShapes: new Shape[0]
+            );
+
+            var renderedText = Rendering.RenderState(state);
+            
+            Assert.That(renderedText, Is.EquivalentTo(new[]
+            {
+                "    ",
+                "    ",
+                "    ",
+                "    ",
+                "    ",
+            }));
+        }
+    }
+}
