b2272382a4744149b7af4757bcfb12e9e220e9d4
Oli Wennell <oli.wennell@gmail.com>
2020-06-19 12:20:31 +0100

    :construction: Hooks everything up for a basic program to try stuff out for real

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace App
 {
@@ -6,7 +7,26 @@ namespace App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var world = new World(
+                width: 5,
+                height: 5,
+                pendingShapes: new Shape[0],
+                fallingShape: new Shape(new []{ new Point(2, 5) }),
+                staticShapes: new Shape[0]);
+
+            
+            for (int i = 0; i < 10; i++)
+            {
+                world = Game.Step(world);
+                var renderedWorld = Rendering.RenderWorld(world);
+                foreach (var line in renderedWorld.Reverse())
+                {
+                    Console.WriteLine(line);
+                }
+                
+                Console.WriteLine("------------------------------");
+                Console.ReadLine();
+            }
         }
     }
 }
